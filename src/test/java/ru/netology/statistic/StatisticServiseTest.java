9a0d7644fd442ed997784ae7c4d84dedb1cd82cc
Anton Berdnikov <ant17893@mail.ru>
2020-05-31 19:02:56 +0600

    first commit

---
+++ src/test/java/ru/netology/statistic/StatisticServiseTest.java
@@ -0,0 +1,58 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatsServiceTest {
+
+    int[] monthlySales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+    void calcAllSalesSum() {
+        StatisticServise service = new StatisticServise();
+        int expected = 180;
+        int actual = service.calcAllSalesSum(monthlySales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcAverageSum() {
+        StatisticServise service = new StatisticServise();
+        int expected = 15;
+        int actual = service.calcAverageSum(monthlySales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcMaxMonthSales() {
+        StatisticServise service = new StatisticServise();
+        int expected = 8;
+        int actual = service.calcMaxMonthSales(monthlySales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcMinMonthSales() {
+        StatisticServise service = new StatisticServise();
+        int expected = 9;
+        int actual = service.calcMinMonthSales(monthlySales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcMonthsBelowAverage() {
+        StatisticServise service = new StatisticServise();
+        int expected = 5;
+        int actual = service.calcMonthsBelowAverage(monthlySales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcMonthsAboveAverage() {
+        StatisticServise service = new StatisticServise();
+        int expected = 5;
+        int actual = service.calcMonthsAboveAverage(monthlySales);
+        assertEquals(expected, actual);
+    }
+}
